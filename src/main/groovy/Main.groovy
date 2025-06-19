00d61dc0f639432b1262203b0b0dbdee23806e31
Hood_16 <112199876+AndreyTsitnarskiy@users.noreply.github.com>
2025-06-19 19:40:14 +0500

    test lib init

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,11 @@
+static void main(String[] args) {
+  println "Hello world!"
+}
+
+static void startStage(String stageName) {
+  println "==================== Start stage " + stageName _+ " ===================="
+}
+
+static void endtStage(String stageName) {
+  println "==================== End stage " + stageName _+ " ===================="
+}
